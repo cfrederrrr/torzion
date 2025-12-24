6067042aaf120925ae8a1db99d282bdbdbb17e0e
dusty <galvertez@gmail.com>
2025-12-24 02:42:24 -0500

    remove unused

--- src/app-tools.zig
+++ src/app-tools.zig
@@ -6,6 +6,8 @@ const tty = std.Io.tty;
 const stdout = std.fs.File.stdout;
 const stderr = std.fs.File.stderr;
 
+const torzion = @import("torzion/MetaInfo.zig");
+
 const LogLevel = enum {
     /// Error: something has gone wrong. This might be recoverable or might
     /// be followed by the program exiting.
@@ -17,7 +19,7 @@ const LogLevel = enum {
     info,
     /// Debug: messages only useful for debugging.
     debug,
-    /// Plain: for simply logging formatted text
+    /// Plain: for simply logging formatted text without any prefix
     plain,
 
     /// Returns a string literal of the given level in full text form.
@@ -50,43 +52,13 @@ pub fn log(
     interface.flush() catch {};
 }
 
-// pub fn defaultLogFn(
-//     comptime level: LogLevel,
-//     comptime scope: @EnumLiteral(),
-//     comptime format: []const u8,
-//     args: anytype,
-// ) void {
-//     _ = std.log.Level; // for reference
-//
-//     var buffer: [64]u8 = undefined;
-//     const stderr, const ttyconf = std.debug.lockStderrWriter(&buffer);
-//     defer std.debug.unlockStderrWriter();
-//     ttyconf.setColor(stderr, switch (level) {
-//         .err => .red,
-//         .warn => .yellow,
-//         .info => .green,
-//         .debug => .magenta,
-//     }) catch {};
-//     ttyconf.setColor(stderr, .bold) catch {};
-//     stderr.writeAll(level.asText()) catch return;
-//     ttyconf.setColor(stderr, .reset) catch {};
-//     ttyconf.setColor(stderr, .dim) catch {};
-//     ttyconf.setColor(stderr, .bold) catch {};
-//     if (scope != .default) {
-//         stderr.print("({s})", .{@tagName(scope)}) catch return;
-//     }
-//     stderr.writeAll(": ") catch return;
-//     ttyconf.setColor(stderr, .reset) catch {};
-//     stderr.print(format ++ "\n", args) catch return;
-// }
-
 pub fn die(comptime message: []const u8, args: anytype, code: u8) noreturn {
     log(.err, message, args);
     std.process.exit(code);
 }
 
-pub fn streql(a: []const u8, b: []const u8) bool {
-    return std.mem.eql(u8, a, b);
-}
-
 pub fn option() !void {}
+
+pub fn readConfigFile(input: []const u8, torrent: *Metainfo) !void {
+    //
+}
