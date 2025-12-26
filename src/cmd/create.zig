755e8e46772c881f84bcdc152bb74ae0df9cfe68
dusty <galvertez@gmail.com>
2025-12-25 20:08:34 -0500

    use toml

--- src/cmd/create.zig
+++ src/cmd/create.zig
@@ -1,14 +1,12 @@
 const std = @import("std");
 
+const toml = @import("toml");
 const cli = @import("cli");
 const clitools = @import("../app-tools.zig");
-const help = clitools.help;
 const die = clitools.die;
-const streql = clitools.streql;
 
 const torzion = @import("torzion");
 const Metainfo = torzion.Metainfo;
-
 const Encoder = torzion.Bencoder;
 
 const Allocator = std.mem.Allocator;
@@ -22,28 +20,22 @@ var torrent: Metainfo = .{ .info = .{ .name = &[_]u8{} } };
 
 // config options provided via cmdline
 var path: []const u8 = ".";
-var announce: [][]const u8 = &.{};
+var cfg: []const u8 = "torrent.toml";
 var out: []const u8 = "-"; // default to stdout
+// var announce: [][]const u8 = &.{};
 
 pub fn command(runner: *cli.AppRunner) !cli.Command {
     return cli.Command{
         .name = "create",
         .description = .{
-            .one_line = "torzion",
+            .one_line = "torzion create", // probably rename to archive
         },
         .options = try runner.allocOptions(&.{
             cli.Option{
-                .short_alias = 'l',
-                .long_name = "piece-length",
-                .value_ref = runner.mkRef(&torrent.info.@"piece length"),
-                .help = "Piece length",
-            },
-            cli.Option{
-                .short_alias = 'a',
-                .long_name = "announce",
-                .required = true,
-                .value_ref = runner.mkRef(&announce),
-                .help = "Comma separated tier of an announce list. Specify --announce more than once for multiple tiers",
+                .short_alias = 'c',
+                .long_name = "config",
+                .value_ref = runner.mkRef(&cfg),
+                .help = "Config file (toml format)",
             },
             cli.Option{
                 .short_alias = 'o',
@@ -51,12 +43,25 @@ pub fn command(runner: *cli.AppRunner) !cli.Command {
                 .value_ref = runner.mkRef(&out),
                 .help = "Output .torrent file",
             },
-            cli.Option{
-                .short_alias = 'p',
-                .long_name = "private",
-                .value_ref = runner.mkRef(&torrent.info.private),
-                .help = "Whether the torrent should be marked private - default is false",
-            },
+            // cli.Option{
+            //     .short_alias = 'l',
+            //     .long_name = "piece-length",
+            //     .value_ref = runner.mkRef(&torrent.info.@"piece length"),
+            //     .help = "Piece length",
+            // },
+            // cli.Option{
+            //     .short_alias = 'a',
+            //     .long_name = "announce",
+            //     .required = true,
+            //     .value_ref = runner.mkRef(&announce),
+            //     .help = "Comma separated tier of an announce list. Specify --announce more than once for multiple tiers",
+            // },
+            // cli.Option{
+            //     .short_alias = 'p',
+            //     .long_name = "private",
+            //     .value_ref = runner.mkRef(&torrent.info.private),
+            //     .help = "Whether the torrent should be marked private - default is false",
+            // },
         }),
         .target = cli.CommandTarget{
             .action = cli.CommandAction{
@@ -80,27 +85,13 @@ pub fn run() !void {
     defer arena.deinit();
     const allocator = arena.allocator();
 
-    var announce_list = try allocator.alloc([][]const u8, announce.len);
+    var parser = toml.Parser(Metainfo).init(allocator);
+    defer parser.deinit();
 
-    var t: usize = 0;
-    for (announce) |tier| {
-        // don't support more than 5 trackers per tier
-        // mainly because nobody needs this, but also because it's easier
-        // and less annoying than making an allocation
-        var trackers: [5][]const u8 = undefined;
-        var it = std.mem.splitScalar(u8, tier, ',');
-        var i: usize = 0;
-        while (it.next()) |tracker| {
-            trackers[i] = tracker;
-            i += 1;
-        }
+    var result = try parser.parseFile(cfg);
+    defer result.deinit();
 
-        // trackers needs to persis outside this scope to be part of announce_list so maybe we need
-        // to allocate after all
-        t += 1;
-    }
-
-    torrent.@"announce-list" = announce_list;
+    torrent = result.value;
 
     const cwd = std.fs.cwd();
     const stat = try cwd.statFile(path);
