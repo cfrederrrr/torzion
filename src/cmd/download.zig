dddeff8eb6ed64dd8ff0095f750b6a1fc237629f
dusty <galvertez@gmail.com>
2026-01-03 03:27:02 -0500

    add download command

--- src/cmd/download.zig
+++ src/cmd/download.zig
@@ -16,11 +16,12 @@ const log = std.log;
 const Self = @This();
 
 // config options provided via cmdline
-var torrentfile: []const u8 = undefined;
+var torrentfile: []const u8 = &.{};
+var outpath: []const u8 = ".";
 
 pub fn command(runner: *cli.AppRunner) !cli.Command {
     return cli.Command{
-        .name = "inspect",
+        .name = "download",
         .description = .{
             .one_line = "torzion",
         },
@@ -32,8 +33,14 @@ pub fn command(runner: *cli.AppRunner) !cli.Command {
                     .required = try runner.allocPositionalArgs(&.{
                         cli.PositionalArg{
                             .value_ref = runner.mkRef(&torrentfile),
-                            .name = "path",
-                            .help = "torrent file",
+                            .name = "torrent file",
+                        },
+                    }),
+                    .optional = try runner.allocPositionalArgs(&.{
+                        cli.PositionalArg{
+                            .value_ref = runner.mkRef(&outpath),
+                            .name = "download path",
+                            .help = "default is the current working directory (.)",
                         },
                     }),
                 },
@@ -43,63 +50,27 @@ pub fn command(runner: *cli.AppRunner) !cli.Command {
 }
 
 pub fn run() !void {
-    const allocator = blk: switch (builtin.mode) {
-        .Debug => {
-            var dba = std.heap.DebugAllocator(.{}).init;
-            break :blk dba.allocator();
-        },
-        else => std.heap.smp_allocator,
-    };
+    const allocator = std.heap.smp_allocator;
 
     const wd = std.fs.cwd();
     const stat = try wd.statFile(torrentfile);
 
-    const fc = try allocator.alloc(u8, stat.size);
-    defer allocator.free(fc);
-    _ = try wd.readFile(torrentfile, fc);
+    const message = try allocator.alloc(u8, stat.size);
+    defer allocator.free(message);
+    _ = try wd.readFile(torrentfile, message);
 
-    var decoder = torzion.Bdecoder{ .message = fc };
-    defer decoder.deinit();
-    const mi: Metainfo = .{};
+    var decoder = torzion.Bdecoder{ .message = message };
+    var mi: Metainfo = .{};
 
     var owner = std.heap.ArenaAllocator.init(std.heap.page_allocator);
 
-    const torrent = decoder.decode(&mi, owner.allocator()) catch |e| switch (e) {
+    decoder.decode(&mi, owner.allocator()) catch |e| switch (e) {
         torzion.Bdecoder.Error.InvalidCharacter => die("Invalid character '{c}' at index {d}", .{ decoder.char(), decoder.cursor }, 1),
         torzion.Bdecoder.Error.UnexpectedToken => die("Invalid character '{c}' at index {d}", .{ decoder.char(), decoder.cursor }, 1),
         else => return e,
     };
 
-    const stdout = std.io.getStdOut().writer();
-    _ = try stdout.write("got here");
-
-    if (torrent.announce) |announce|
-        _ = try std.fmt.format(stdout, "announce: {s}\n", .{announce});
-
-    if (torrent.@"announce-list") |list| {
-        _ = try stdout.write("announce-list:\n");
-        for (list) |tier| {
-            for (tier) |announce| _ = try std.fmt.format(stdout, "  - {s}\n", .{announce});
-        }
-    }
-
-    _ = try stdout.write("info:\n");
-    _ = try std.fmt.format(stdout, "  name: {s}\n", .{torrent.info.name});
-    _ = try std.fmt.format(stdout, "  piece length: {d}\n", .{torrent.info.@"piece length"});
-
-    if (torrent.info.files) |files| {
-        _ = try stdout.write("  files:\n");
-        for (files) |file| {
-            const pretty_path = try std.mem.join(allocator, "/", file.path);
-            _ = try std.fmt.format(stdout, "  - path: {s}\n", .{pretty_path});
-            allocator.free(pretty_path);
-            _ = try std.fmt.format(stdout, "  - length: {d}\n", .{file.length});
-        }
-    }
-
-    if (torrent.info.length) |length| {
-        try std.fmt.format(stdout, "  length: {d}", .{length});
-    }
+    // torzion.joinSwarm();
 }
 
 pub fn action() anyerror!void {
