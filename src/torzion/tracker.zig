1c859d27bf875253b050d120752045252474fa18
dusty <galvertez@gmail.com>
2026-01-03 03:29:02 -0500

    * tracker is stateful now * state is tracked with State * announce uses State

--- src/torzion/tracker.zig
+++ src/torzion/tracker.zig
@@ -2,49 +2,61 @@
 const std = @import("std");
 const http = std.http;
 
+const Tracker = @This();
+const Allocator = std.mem.Allocator;
 const Metainfo = @import("Metainfo.zig");
-
 const Decoder = @import("Bdecoder.zig");
+const Encoder = @import("Bencoder.zig");
+const Peer = @import("protocol.zig").Peer;
 
-pub const Error = error{
-    NoAnnounceDefined,
-    CouldNotReadBody,
-    ContentLengthNotSpecified,
-    CouldNotContactTracker,
-    AnnounceFailure,
-};
+meta_info: Metainfo,
+peers: []Peer = &.{},
+state: State,
 
-// TODO:
-// This needs to be a union of Failure and OK
-// which means we have to write the bdecoder for unions now
-pub const Response = union(enum) {
-    failure: Failure,
-    ok: OK,
+/// An optional parameter giving the IP (or dns name) which this peer is at.
+/// Generally used for the origin if it's on the same machine as the tracker.
+ip: ?[]const u8 = null,
 
-    pub const Failure = struct {
-        /// If a tracker response has a key failure reason, then that maps to a human
-        /// readable string which explains why the query failed, and no other keys are
-        /// required.
-        @"failure reason": []const u8,
+pub fn init(message: []const u8, owner: Allocator) Tracker {
+    var meta_info = Metainfo{};
+
+    // TODO:
+    // implement decoder.decode such that it validates that fields are sorted
+    var decoder = Decoder{ .message = message };
+    try decoder.decode(&meta_info, owner);
+
+    var encoder = Encoder{ .message = decoder.message, .allocator = owner };
+    try encoder.encode(meta_info.info);
+
+    var state: State = .{};
+    std.crypto.random.bytes(&state.peer_id);
+    std.crypto.hash.Sha1.hash(encoder.message, &state.info_hash, .{});
+
+    // TODO:
+    // 1. determine the port
+    // 2. figure out how to check an existing download to populate
+    //    - uploaded
+    //    - downloaded
+    //    - left
+
+    return .{
+        .meta_info = meta_info,
+        .state = state,
     };
+}
 
-    pub const OK = struct {
-        /// Otherwise, it must have two keys: interval, which maps to the number of
-        /// seconds the downloader should wait between regular rerequests,
-        interval: u32,
+pub fn deinit(self: *Tracker, owner: Allocator) void {
+    self.meta_info.deinit(owner);
+    owner.free(self.torrent);
+    if (self.peers.len > 1) {
+        // this might be unnecessary - might be best to store the latest response text
+        // raw and just use the IPs there in which case an allocation is pointless
+        for (self.peers) |*peer| owner.free(peer.*.ip);
+        owner.free(self.peers);
+    }
+}
 
-        // /// and peers. peers maps to a list of dictionaries corresponding to peers,
-        // /// each of which contains the keys peer id, ip, and port, which map to the
-        // /// peer's self-selected ID, IP address or dns name as a string, and port
-        // /// number, respectively. Note that downloaders may rerequest on
-        // /// nonscheduled times if an event happens or they need more peers.
-        // peers: []Peer,
-    };
-};
-
-pub const Announcement = struct {
-    pub const Event = enum { started, completed, stopped, empty };
-
+const State = struct {
     /// The 20 byte sha1 hash of the bencoded form of the info value from the metainfo file.
     /// This value will almost certainly have to be escaped.
     /// Note that this is a substring of the metainfo file. The info-hash must be the hash
@@ -53,33 +65,29 @@ pub const Announcement = struct {
     /// bdecoder fully validated the input (e.g. key ordering, absence of leading zeros).
     /// Conversely that means clients must either reject invalid metainfo files or extract
     /// the substring directly. They must not perform a decode-encode roundtrip on invalid data.
-    info_hash: [20]u8,
+    info_hash: [20]u8 = undefined,
 
     /// A string of length 20 which this downloader uses as its id. Each downloader generates
     /// its own id at random at the start of a new download. This value will also almost
     /// certainly have to be escaped.
-    peer_id: [20]u8,
-
-    /// An optional parameter giving the IP (or dns name) which this peer is at.
-    /// Generally used for the origin if it's on the same machine as the tracker.
-    ip: []const u8,
+    peer_id: [20]u8 = undefined,
 
     /// The port number this peer is listening on. Common behavior is for a downloader to
     /// try to listen on port 6881 and if that port is taken try 6882, then 6883, etc. and
     /// give up after 6889.
-    port: u16,
+    port: u16 = 6881,
 
     /// The total amount uploaded so far, encoded in base ten ascii.
-    uploaded: usize,
+    uploaded: usize = 0,
 
     /// The total amount downloaded so far, encoded in base ten ascii.
-    downloaded: usize,
+    downloaded: usize = 0,
 
     /// The number of bytes this peer still has to download, encoded in base ten ascii.
     /// Note that this can't be computed from downloaded and the file length since it might
     /// be a resume, and there's a chance that some of the downloaded data failed an
     /// integrity check and had to be re-downloaded.
-    left: usize,
+    left: usize = 0,
 
     /// This is an optional key which maps to
     /// - started
@@ -92,102 +100,114 @@ pub const Announcement = struct {
     /// completed is sent when the download is complete. No completed is sent if the file
     /// was complete when started. Downloaders send an announcement using stopped when they
     /// cease downloading.
-    event: ?Event,
-
-    const format = "info_hash={s}&peer_id={s}&ip={s}&port={d}&uploaded={s}&downloaded={s}&left={s}&event={s}";
-
-    pub fn toString(self: Announcement, allocator: std.mem.Allocator) ![]u8 {
-        return try std.fmt.allocPrint(allocator, format, .{
-            self.info_hash,
-            self.peer_id,
-            self.ip,
-            self.port,
-            self.uploaded,
-            self.downloaded,
-            self.left,
-            @tagName(self.event orelse .empty),
-        });
-    }
+    event: ?Event = .empty,
 };
 
-pub fn announce(allocator: std.mem.Allocator, meta_info: Metainfo, announcement: Announcement) ![]Response {
-    //
-    var client = http.Client{ .allocator = allocator };
-    defer client.deinit();
+pub const Response = struct {
+    /// If a tracker response has a key failure reason, then that maps to a human
+    /// readable string which explains why the query failed, and no other keys are
+    /// required.
+    failure_reason: ?[]const u8,
 
+    /// Otherwise, it must have two keys: interval, which maps to the number of
+    /// seconds the downloader should wait between regular rerequests,
+    interval: ?u32,
+
+    /// and peers. peers maps to a list of dictionaries corresponding to peers,
+    /// each of which contains the keys peer id, ip, and port, which map to the
+    /// peer's self-selected ID, IP address or dns name as a string, and port
+    /// number, respectively. Note that downloaders may rerequest on
+    /// nonscheduled times if an event happens or they need more peers.
+    peers: ?[]Peer,
+
+    pub const WireNames = .{ .failure_reason = "failure reason" };
+};
+
+pub const Event = enum {
+    started,
+    completed,
+    stopped,
+    empty,
+};
+
+pub const AnnounceError = error{
+    NoAnnounceDefined,
+    CouldNotReadBody,
+    ContentLengthNotSpecified,
+    CouldNotContactTracker,
+    AnnounceFailure,
+};
+
+pub fn announce(self: *Tracker, allocator: std.mem.Allocator, event: ?Event) ![]Response {
     const responses = std.ArrayList(Response).init(allocator);
     defer responses.deinit();
 
-    if (meta_info.@"announce-list") |list| {
-        for (list) |tier| {
-            for (tier) |url| {
-                const response = sendAnnouncement(allocator, client, url, announcement) catch continue;
-                try responses.append(response);
-                switch (response) {
-                    .ok => break,
-                    .failure => continue,
-                }
-            }
-        }
-    } else if (meta_info.announce) |url| {
-        const response = try sendAnnouncement(allocator, client, url, announcement);
+    var threaded = std.Io.Threaded.init(allocator);
+    defer threaded.deinit();
+    const io = threaded.io();
+    const client: std.http.Client = .{ .allocator = allocator, .io = io };
+
+    if (self.meta_info.announce_list) |list| {
+        for (list) |tier| for (tier) |url| {
+            const uri = std.Uri.parse(url);
+            const response = sendAnnouncement(allocator, client, uri, event) catch continue;
+            try responses.append(response);
+            // WARN: are we supposed to continue here?
+            // if (response.failure_reason != null) continue else break;
+        };
+    } else if (self.meta_info.announce) |url| {
+        const response = try sendAnnouncement(allocator, client, url, event);
         try responses.append(response);
     } else {
-        return Error.NoAnnounceDefined;
+        return AnnounceError.NoAnnounceDefined;
     }
 
     return responses.toOwnedSlice();
 }
 
-fn sendAnnouncement(allocator: std.mem.Allocator, client: http.Client, url: []const u8, announcement: Announcement) !Response {
-    //
-    var uri = try std.Uri.parse(url);
-    const query = try announcement.toString(allocator);
-    defer allocator.free(query);
-    uri.query = .{ .raw = query };
+fn sendAnnouncement(self: *Tracker, allocator: std.mem.Allocator, client: http.Client, url: []const u8, event: ?Event) !Response {
+    var location = try std.Io.Writer.Allocating.initCapacity(allocator, url.len);
+    try location.writer.write(url);
 
-    const request = client.open(.GET, uri, .{}) catch return Error.CouldNotContactTracker;
-    defer request.deinit();
-    try request.send();
-    try request.finish();
-    try request.wait();
+    try location.print("?info_hash=", .{});
+    try std.Uri.Component.percentEncode(location, self.info_hash, struct {
+        pub fn f(_: u8) bool {
+            return false;
+        }
+    }.f);
 
-    // we don't talk to servers that won't tell us the content length
-    const content_length = request.response.content_length orelse return Error.ContentLengthNotSpecified;
-    const body = allocator.alloc(u8, content_length);
+    try location.print("&peer_id={s}&port={d}&uploaded={d}&downloaded={d}&left={d}", .{
+        self.state.peer_id,
+        self.state.port,
+        self.state.uploaded,
+        self.state.downloaded,
+        self.state.left,
+    });
 
-    _ = request.read(body) catch return Error.CouldNotReadBody;
+    if (self.ip) |ip|
+        location.print("&ip={s}", .{ip});
 
-    var decoder = try Decoder.init(allocator, body);
-    // WARN:
-    // we can't deinit here, or else we have to
-    // a) return the decoder instead of the response
-    // b) copy the data from the decoder to a new response object
-    // but we also have to deinit because otherwise this fn leaks a whole decoder
-    // and its message every time.
-    //
-    // idk what to do yet. maybe we shouldn't be decoding the response here.
-    // this function should probably just handle the networking/http parts
-    // and return the raw string to be decoded
-    //
-    // on the other hand, i don't think i want the decoder to own the memory anymore
-    // but that would mean either carrying an allocator around with all the decoded
-    // entitites, or tracking them elsewhere
-    //
-    // do we empower the decoder to decode multiple messages and track ownership there?
-    // no way.
-    //
-    // maybe the decoder should have a `.decodeOwned(T, item) !void` method that parses and
-    // then copies the data to a provided item rather than returning it. or maybe the
-    // .decode function should just have that behavior by default.
-    defer decoder.deinit();
-    const ok = decoder.decode(Response.OK) catch return decoder.decode(Response.Failure);
+    if (event) |_|
+        location.print("&event={s}", .{@tagName(event)});
 
-    return Response{ .ok = ok };
-}
+    var body = std.Io.Writer.Allocating.init(allocator);
+    defer body.deinit();
 
-fn warn(string: []const u8, args: anytype) void {
-    _ = string;
-    _ = args;
-    return;
+    var redirect_buffer: [0x2000]u8 = undefined;
+    const fetch_result = try client.fetch(.{
+        .method = .GET,
+        .location = .{ .url = location.written() },
+        .redirect_buffer = &redirect_buffer,
+        .response_writer = &body,
+    });
+
+    if (fetch_result.status != .ok) {
+        return AnnounceError.AnnounceFailure;
+    }
+
+    var decoder = Decoder{ .message = body.written() };
+    var response: Response = .{};
+    try decoder.decode(&response, allocator);
+
+    return response;
 }
